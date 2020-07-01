5b27dfd24cd227ee7d1fa8d45b1bcbe1416c6a26
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2020-07-01 17:22:10 -0300

    Adding api connection

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,65 +1,65 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './style';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    evt: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    evt.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <input type="text" placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="#">
-          <img
-            src="https://avatars0.githubusercontent.com/u/8530623?s=400&u=431b427d3a26b947dd486bfd02e60b39e03caa50&v=4"
-            alt="Avatar"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>
-              Aut enim voluptatem nam quam. Voluptas id vel voluptate quisquam.
-              Delectus maxime voluptas aut nesciunt quia eveniet.
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="#">
-          <img
-            src="https://avatars0.githubusercontent.com/u/8530623?s=400&u=431b427d3a26b947dd486bfd02e60b39e03caa50&v=4"
-            alt="Avatar"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>
-              Aut enim voluptatem nam quam. Voluptas id vel voluptate quisquam.
-              Delectus maxime voluptas aut nesciunt quia eveniet.
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="#">
-          <img
-            src="https://avatars0.githubusercontent.com/u/8530623?s=400&u=431b427d3a26b947dd486bfd02e60b39e03caa50&v=4"
-            alt="Avatar"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>
-              Aut enim voluptatem nam quam. Voluptas id vel voluptate quisquam.
-              Delectus maxime voluptas aut nesciunt quia eveniet.
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="#">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );

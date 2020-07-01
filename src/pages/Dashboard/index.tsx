e5ad7b883b7a06d20001740307c3c368d4a14cfd
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2020-07-01 16:40:19 -0300

    Adding Dashboard style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './style';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './style';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/8530623?s=400&u=431b427d3a26b947dd486bfd02e60b39e03caa50&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Aut enim voluptatem nam quam. Voluptas id vel voluptate quisquam.
+              Delectus maxime voluptas aut nesciunt quia eveniet.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/8530623?s=400&u=431b427d3a26b947dd486bfd02e60b39e03caa50&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Aut enim voluptatem nam quam. Voluptas id vel voluptate quisquam.
+              Delectus maxime voluptas aut nesciunt quia eveniet.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/8530623?s=400&u=431b427d3a26b947dd486bfd02e60b39e03caa50&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Aut enim voluptatem nam quam. Voluptas id vel voluptate quisquam.
+              Delectus maxime voluptas aut nesciunt quia eveniet.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
